7e8850f540f7696fc8ba657b3249cc3c8b9fc648
iamcurrent <519719171@qq.com>
2021-03-10 14:21:02 +0800

    update

--- src/main/java/com/example/pro1/demo/DemoApplication.java
+++ src/main/java/com/example/pro1/demo/DemoApplication.java
@@ -2,8 +2,11 @@ package com.example.pro1.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySource("classpath:datasourceconfig.properties")
 public class DemoApplication {
 
     public static void main(String[] args) {
